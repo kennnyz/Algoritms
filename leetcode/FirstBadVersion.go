e4ef83ab14647fdd67beaf35e6bcd154194e840a
Muhammed <muhammed1410@yandex.ru>
2022-12-03 13:27:49 +0300

    FirstBadVersion commit

---
+++ leetcode/FirstBadVersion.go
@@ -0,0 +1,24 @@
+package leetcode
+
+var BV int = 4
+
+func isBadversion(n int) bool {
+	return n >= BV
+}
+func FirstBadVersion(n int) int {
+	left := 0
+	right := n
+
+	for left < right {
+		midpoint := left + (right-left)/2
+		if isBadversion(midpoint) {
+			right = midpoint
+		} else {
+			left = midpoint + 1
+		}
+	}
+	if left == right && isBadversion(left) {
+		return left
+	}
+	return -1
+}
--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/kennyz/Algoritms/leetcode"
 )
 
+var pop int = 4
+
+func isBadversion(n int) bool {
+	if n >= pop {
+		return true
+	}
+	return false
+}
 func main() {
 	a := []int{1, 2, 3, 4, 5, 6}
-	fmt.Println(leetcode.Search(a, 6))
+	fmt.Println(isBadversion(a[3]))
 }
